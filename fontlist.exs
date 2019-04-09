ea6c4a6dcda726f184a9d7321f6cc729345725a6
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-04-09 08:04:37 +0100

    Abort (crash?) if exit status is not 0
    Something something match error. Voodoo.

--- fontlist.exs
+++ fontlist.exs
@@ -1,7 +1,7 @@
 require Logger
 
 Logger.info "Fetching list of system fonts..."
-{fonts, retval} = System.cmd "fc-list", []
+{fonts, 0} = System.cmd "fc-list", []
 fonts = String.split fonts, "\n"
 
 IO.inspect fonts
