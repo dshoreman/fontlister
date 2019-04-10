60aa4abc0792c7319660240f0a708a4ac8177e50
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-04-10 04:39:34 +0100

    Wrap everything in a module

--- fontlist.exs
+++ fontlist.exs
@@ -1,11 +1,18 @@
-require Logger
+defmodule Fonts do
+  require Logger
 
-Logger.info "Fetching list of system fonts..."
-{fonts, 0} = System.cmd "fc-list", []
-fonts = String.split fonts, "\n"
+  @doc "Get list of system fonts from Fontconfig"
+  def list do
+    Logger.info "Fetching list of system fonts..."
+    {fonts, 0} = System.cmd "fc-list", []
+    fonts = String.split fonts, "\n"
 
-Logger.info "Parsing fonts..."
-for font <- fonts do
-  [path, family, style] = String.split font, ":"
-  Logger.debug "Font Path: " <> path <> "\nFamily: " <> family <> "\nStyle: " <> style
+    Logger.info "Parsing fonts..."
+    for font <- fonts do
+      [path, family, style] = String.split font, ":"
+      Logger.debug "Font Path: " <> path <> "\nFamily: " <> family <> "\nStyle: " <> style
+    end
+  end
 end
+
+Fonts.list()
