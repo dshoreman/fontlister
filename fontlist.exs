04a22422da44e0d96bba17640ea6865ea877553c
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-04-10 07:31:23 +0100

    Handle exit status > 0
    Uses pipes to pass things down the line into new private split_lines
    function which takes the original {fonts, retval} tuple. If the response
    doesn't match the 0 exit status that the first defp wants, it'll fall
    back to the second where it raises an exception.

--- fontlist.exs
+++ fontlist.exs
@@ -4,14 +4,23 @@ defmodule Fonts do
   @doc "Get list of system fonts from Fontconfig"
   def list do
     Logger.info "Fetching list of system fonts..."
-    {fonts, 0} = System.cmd "fc-list", []
-    fonts = String.split fonts, "\n"
+    fonts = "fc-list"
+    |> System.cmd([], stderr_to_stdout: true)
+    |> split_lines
 
     Logger.info "Parsing fonts..."
     for font <- fonts do
       [path, family, style] = String.split font, ":"
       Logger.debug "Font Path: " <> path <> "\nFamily: " <> family <> "\nStyle: " <> style
     end
+  end
+
+  defp split_lines ({fonts, 0}) do
+    String.split(fonts, "\n")
+  end
+
+  defp split_lines (_) do
+    raise "Failed to get font list."
   end
 end
 
